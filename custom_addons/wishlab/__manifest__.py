3183c9dc692bd17ab17a4165ce9b2dd56ed6e58e
effaz <effaz.rayhan@brainstation-23.com>
2026-05-18 14:59:25 +0600

    Initial Commit

---
+++ custom_addons/wishlab/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Wish Labs',
+    'version': '1.0.0',
+    'depends': [],
+    'author': 'BRAIN STATION 23',
+    'description': """
+        A module to store birthdays and automatically wish the users through mail.
+    """,
+    'data': [],
+    'application': True,
+    'installable': True,
+}
