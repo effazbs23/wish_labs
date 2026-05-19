ff6385fc6d5ef7537ff604fb591774f1fb4070c3
effaz <effaz.rayhan@brainstation-23.com>
2026-05-19 16:13:25 +0600

    [ADD] wishlab: Added wish lab logic and backend

---
+++ custom_addons/wishlab/models/wish_lab.py
@@ -0,0 +1,11 @@
+
+from odoo import fields,models
+
+class WishLab(models.Model):
+    _name="wish.lab"
+    _description="Wish Lab Model"
+
+    name = fields.Char()
+    email = fields.Char()
+    connection_date = fields.Date()
+    birthday = fields.Date()
