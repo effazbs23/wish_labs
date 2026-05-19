90e6109a434e19886b5f8fd8614713517da08f0b
effaz <effaz.rayhan@brainstation-23.com>
2026-05-19 16:47:08 +0600

    [ADD] wishlab: Added functionality to fetch partner info from res.partner

--- custom_addons/wishlab/models/wish_lab.py
+++ custom_addons/wishlab/models/wish_lab.py
@@ -5,7 +5,8 @@ class WishLab(models.Model):
     _name='wish.lab'
     _description='Wish Lab Model'
 
-    name = fields.Char()
-    email = fields.Char()
-    connection_date = fields.Date()
+    partner_id = fields.Many2one('res.partner',string="Partner ID")
+    name = fields.Char(related="partner_id.name")
+    email = fields.Char(related="partner_id.email")
+    connection_date = fields.Datetime(related="partner_id.create_date")
     birthday = fields.Date()
